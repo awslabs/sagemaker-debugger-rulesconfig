0724619f4d2d38e5961ddea73447cc809aa5c93c
Amol Lele <19983848+leleamol@users.noreply.github.com>
2019-12-03 11:57:16 -0800

    Updated the setup.py with long description and updated the version

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 # Standard Library
-import os
-import sys
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+long_description = (
+    "This library, intended to be used with Amazon SageMaker Python SDK, helps you specify builtin "
+    "rules without worrying about any details or tweak the configuration of builtin rules. These "
+    "builtin rules are available in Amazon SageMaker."
+    "Amazon SageMaker Debugger Rulesconfig package can be used with Amazon SageMaker Debugger or as"
+    "stand-alone rule config retriever. In addition to retrieving builtin rules, configuration for common collections can be retrieved as well."
+)
 
 exec(open("smdebug_rulesconfig/_version.py").read())
 CURRENT_VERSION = __version__
+
 
 def build_rule_config_package(version):
     setuptools.setup(
@@ -16,10 +20,9 @@ def build_rule_config_package(version):
         author="AWS DeepLearning Team",
         description="SMDebug RulesConfig",
         long_description=long_description,
-        long_description_content_type="text/markdown",
         url="https://github.com/awslabs/sagemaker-debugger-rulesconfig",
         packages=["smdebug_rulesconfig"],
-        package_data={'smdebug_rulesconfig' : ['rule_config_jsons/*.json']},
+        package_data={"smdebug_rulesconfig": ["rule_config_jsons/*.json"]},
         classifiers=[
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: Apache Software License",
@@ -28,5 +31,5 @@ def build_rule_config_package(version):
         python_requires=">=2.7",
     )
 
-build_rule_config_package(version=CURRENT_VERSION)
 
+build_rule_config_package(version=CURRENT_VERSION)
