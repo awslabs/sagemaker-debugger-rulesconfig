1822d0918cd999e098b9bc20f9a867e5bf8e22f6
ndodda-amazon <ndodda@amazon.com>
2020-12-17 01:03:54 -0800

    update buildspec/tests.sh to treat core tests as separate build

--- config/tests.sh
+++ config/tests.sh
@@ -5,6 +5,14 @@ set -e
 set -ex
 set -o pipefail
 
+
+run_for_framework() {
+    python -m pytest ${code_coverage_smdebug:+--cov=./ --cov-append}  --durations=50 --html=$REPORT_DIR/report_$1.html -v -s --self-contained-html tests/$1
+}
+
 export OUT_DIR=upload/$CURRENT_COMMIT_PATH
 export REPORT_DIR=$OUT_DIR/pytest_reports
-python -m pytest ${code_coverage_smdebug:+--cov=./ --cov-append}  --durations=50 --html=$REPORT_DIR/report_core.html -v -s --self-contained-html tests/core
+
+if [ "$run_pytest_core" = "enable" ] ; then
+    run_for_framework core
+fi
