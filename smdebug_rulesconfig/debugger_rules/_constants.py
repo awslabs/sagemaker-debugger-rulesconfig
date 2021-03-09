57ba7856e9a0dc0861ae59ad00ec7b7d8aaa0cc2
NihalHarish <nihal42harish@gmail.com>
2021-03-09 02:41:18 -0800

    removed unused json

--- smdebug_rulesconfig/debugger_rules/_constants.py
+++ smdebug_rulesconfig/debugger_rules/_constants.py
@@ -1,5 +1,4 @@
 RULE_CONFIG_FILE = "rule_config_jsons/ruleConfigs.json"
-RULE_GROUPS_CONFIG_FILE = "rule_config_jsons/ruleGroups.json"
 COLLECTION_CONFIG_FILE = "rule_config_jsons/collections.json"
 
 SUPPORTED_DL_FRAMEWORKS = {"TENSORFLOW", "MXNET", "PYTORCH"}
